9995efb8f237cd4ecdade8ec04a644b97a841834
MSoledadSanchez <msoledad.sanchez@hotmail.com>
2025-12-14 19:18:06 -0300

    Ajuste pie

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,16 +10,16 @@ const Footer = () => {
 
                 <p>© 2025 Elevarte. Todos los derechos reservados.</p>
                 
-                <div class="contact-info">
-                <span>📧 Email: info@elevarte.com.ar</span> | 
-                <span>📞 Tel: +54 9 11 1234-5678</span> | 
-                <span>📍 Dirección: San Martin 123, Olivos, Bs As</span>
+                <div className={styles.direccion}>
+                    <span>📧 Email: info@elevarte.com.ar</span> | 
+                    <span>📞 Tel: +54 9 11 1234-5678</span> | 
+                    <span>📍 Dirección: San Martin 123, Olivos, Bs As</span>
                 </div>
 
-                <div class="social-icons">
-                <a href="link_instagram" aria-label="Instagram">📸</a>
-                <a href="link_facebook" aria-label="Facebook">📘</a>
-                <a href="link_whatsapp" aria-label="WhatsApp">💬</a>
+                <div >
+                    <a href="link_instagram" aria-label="Instagram">📸</a>
+                    <a href="link_facebook" aria-label="Facebook">📘</a>
+                    <a href="link_whatsapp" aria-label="WhatsApp">💬</a>
                 </div>
 
             </div>
--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,6 +1,27 @@
 const Contacto = () => {
     return(
-        <h1>Contacto</h1>
+         <>
+            <h2>Contactanos</h2>
+
+                <h3>Nuestro Horarios</h3>
+                    <p>Lunes a viernes: 9hs. a 19hs.</p>
+                    <p>sábado: 9hs. a 13hs.</p>
+                    <p>Domingo: Cerrado</p>
+                <br />
+                <div >
+                    <p>📧 Email: info@elevarte.com.ar</p> 
+                    <p>📞 Tel: +54 9 11 1234-5678</p> 
+                    <p>📍 Dirección: San Martin 123, Olivos, Bs As</p>
+                </div>
+                <br />
+                <div >
+                    <a href="link_instagram" aria-label="Instagram">📸</a>
+                    <a href="link_facebook" aria-label="Facebook">📘</a>
+                    <a href="link_whatsapp" aria-label="WhatsApp">💬</a>
+                </div>
+                 <br />
+
+        </>
     )
 
 }
